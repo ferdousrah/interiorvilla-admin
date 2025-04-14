d258e5becbd39dc31b22f814de49efe2b2061c0d
Ferdus Rahman <ferdous.rahman.tapon@gmail.com>
2025-04-14 16:40:33 +0600

    link removed from footer

--- src/Footer/Component.tsx
+++ src/Footer/Component.tsx
@@ -18,11 +18,7 @@ export async function Footer() {
       <div className="container py-8 gap-8 flex flex-col md:flex-row md:justify-between">
         <div className="flex flex-col-reverse items-start md:flex-row gap-4 md:items-center">
           <ThemeSelector />
-          <nav className="flex flex-col md:flex-row gap-4">
-            {navItems.map(({ link }, i) => {
-              return <CMSLink className="text-white" key={i} {...link} />
-            })}
-          </nav>
+          
         </div>
       </div>
     </footer>
