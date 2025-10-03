b797c464f4fbc08ffa68facb1eed4a2a43b0419b
Ferdus Rahman <ferdous.rahman.tapon@gmail.com>
2025-10-03 22:35:52 +0600

    seoPlugin removed

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -1,8 +1,9 @@
+// src/plugins/index.ts
 import { payloadCloudPlugin } from '@payloadcms/payload-cloud'
 import { formBuilderPlugin } from '@payloadcms/plugin-form-builder'
 import { nestedDocsPlugin } from '@payloadcms/plugin-nested-docs'
 import { redirectsPlugin } from '@payloadcms/plugin-redirects'
-import { seoPlugin } from '@payloadcms/plugin-seo'
+//import { seoPlugin } from '@payloadcms/plugin-seo'
 import { searchPlugin } from '@payloadcms/plugin-search'
 import { Plugin } from 'payload'
 import { revalidateRedirects } from '@/hooks/revalidateRedirects'
@@ -51,10 +52,10 @@ export const plugins: Plugin[] = [
     collections: ['categories'],
     generateURL: (docs) => docs.reduce((url, doc) => `${url}/${doc.slug}`, ''),
   }),
-  seoPlugin({
+  /*seoPlugin({
     generateTitle,
     generateURL,
-  }),
+  }),*/
   formBuilderPlugin({
     fields: {
       payment: false,
